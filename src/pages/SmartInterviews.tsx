92d8a4a42741a644689baafb9fd983e808fcad8c
Sacha Lifszyc <sacha.lifszyc@toptal.com>
2025-04-09 18:21:35 -0300

    Complete Smart Intervews

--- src/pages/SmartInterviews.tsx
+++ src/pages/SmartInterviews.tsx
@@ -1,55 +1,93 @@
-import projectCover from '../assets/images/tiendanube-project-cover.png'
-import designingImage3 from '../assets/images/designing-03.png'
-import designingImage4 from '../assets/images/designing-04.png'
-import designingImage5 from '../assets/images/designing-05.png'
-import tiendanubeImplementation from '../assets/images/tiendanube-implementation-01.png'
-import tiendanubeImplementation2 from '../assets/images/tiendanube-implementation-02.png'
-import tiendanubeImplementation3 from '../assets/images/tiendanube-implementation-03.png'
+import projectCover from '../assets/images/smart-interviews.png'
+import smartInterviewsUnderstanding from '../assets/images/smart-interviews-understanding.png'
+import smartInterviewsImage1 from '../assets/images/smart-interviews-01.png'
+import smartInterviewsImage2 from '../assets/images/smart-interviews-02.png'
+import smartInterviewsImage3 from '../assets/images/smart-interviews-03.png'
+import smartInterviewsImage4 from '../assets/images/smart-interviews-04.png'
 
 import { Page, Section, ProjectCover, StickyNote } from '../components'
 import Box from '../components/Box'
 import { Header } from '../containers'
-import Card from '../components/Card'
 
-function Fingerprint() {
+const designingNotes = [
+  'What if AI could create a score in order to quickly make decisions and compare candidates?',
+  "How could it help order the recruiter's job? What is the next action they need to do?",
+  'What if AI could summarize strengths, and risks?',
+  'What if transcripts were the primary way to navigate a video?',
+  'What if recruiters could approve, dismiss, or edit what the AI suggests?',
+  "How can we generate trust? What if we made the model's “thinking” visible?",
+  'How can we AI help future interviews to make the best out of them?',
+]
+
+const keyDesignDecisions = [
+  {
+    title: 'Make AI reasoning visible',
+    description:
+      'Add contextual links and visual anchors, like highlighted transcript lines or video snippets so recruiters can verify what the AI is basing its insight on. This helps build trust by making the AI’s “thinking” transparent and auditable.',
+  },
+  {
+    title: 'AI suggests, people decide',
+    description:
+      'The final decision should always be made by a person. The role of the AI is to suggest, tidy up the information and make it easily readable.',
+  },
+
+  {
+    title: 'Guide with clear next steps',
+    description:
+      'We used simple UI patterns (progress bars, call-to-action buttons, candidate queues) to reduce ambiguity and help users stay in flow and easily access what they need to complete.',
+  },
+  {
+    title: 'Design for scale',
+    description:
+      'Speed, readability, and efficient comparison tools are important when recruiter need to see many candidates on one day and make decisions.',
+  },
+]
+
+function SmartInterviews() {
   return (
     <Page className='divide-y-2 divide-solid divide-black'>
       <ProjectCover
         preContent={<Header />}
-        year={2019}
-        color='grey'
-        client='Tiendanube'
+        year={2025}
+        color='purple-light'
+        client='Crosschq'
         title='Redesigning Tiendanube’s Admin'
         image={projectCover}
-        imageClassName=''
+        imageClassName='w-[719px] h-[458px]'
       />
 
       <Section className='py-[60px] lg:py-[120px]'>
         <div className='2xl:max-w-[792px] mx-auto'>
           <h2 className='text-large font-medium mb-[12px]'>Overview</h2>
           <p className='text-medium'>
-            In 2020, I led the redesign of Tiendanube s admin experience. The
-            admin panel was used every day by tens of thousands of store owners
-            to manage their products, orders, payments, and more. Over time, the
-            platform grew in complexity and with that, the experience started to
-            feel cluttered, inconsistent, and hard to navigate.
+            At Crosschq, I was asked to explore a new product vertical: an AI
+            agent that could help recruiters analyze video interviews more
+            effectively and objectively.
           </p>
           <p className='text-medium mt-[20px]'>
-            The goal of this project was to rethink the admin’s structure and
-            user experience. Not by adding more features, but by making it
-            easier to understand, simpler to use, and aligned with how people
-            actually work.
+            This was not about polishing an existing feature—it was about
+            imagining and designing a new product experience from the ground up.
+            One that would feel intuitive, helpful, and trustworthy—while also
+            handling sensitive human information.
           </p>
-          <Box color='grey/25' className='mt-[29px]'>
+          <p className='text-medium mt-[20px]'>
+            I led the end-to-end design exploration: from initial concept
+            sketches to high-fidelity prototypes that visualized how this tool
+            could work across different touchpoints.
+          </p>
+          <Box color='purple-light/10' className='mt-[29px]'>
             <h3 className='text-medium font-medium'>My role</h3>
             <p className='text-medium mt-[20px]'>
-              Led the UX and UI design from discovery to implementation:
-              <ul className='list-disc list-inside'>
-                <li>Conducted user interviews and research</li>
-                <li>Defined the new product architecture</li>
-                <li>Designed and tested low-to-high fidelity prototypes</li>
-                <li>Implemented most of the frontend code</li>
-                <li>Supported rollout and feedback loops</li>
+              Led the design exploration for a new product vertical.
+              <ul className='ml-[40px]'>
+                <li>Mapping out the problem space with stakeholders</li>
+                <li>
+                  Designing and prototyping the product vision from scratch
+                </li>
+                <li>
+                  Shaping early conversations around trust, transparency, and
+                  automation
+                </li>
               </ul>
             </p>
           </Box>
@@ -60,172 +98,155 @@ function Fingerprint() {
         <div className='2xl:max-w-[792px] mx-auto'>
           <h3 className='text-large font-medium mb-[12px]'>Understanding</h3>
           <p className='text-medium mt-[20px]'>
-            I started the redesigning of Tiendanube’s admin panel by deeply
-            understanding how the old one was being used.
-          </p>
-          <p className='text-medium mt-[40px]'>
-            I interviewed users who used the platform daily. They often couldn’t
-            find what they were looking for, labels didn’t match their
-            expectations and features were hard to discover. I followed that up
-            with card-sorting exercises, having more than 30 users group
-            different sections of the admin into categories that made sense to
-            them.
-          </p>
-          <p className='text-medium mt-[40px]'>
-            Lastly, I looked at analytics and internal documentation to get the
-            business perspective, what features were critical, how users
-            navigated today, and what the product team expected to scale next.
+            To begin, I needed to understand the reality of recruiter workflows,
+            what they value and what they need to see.
           </p>
         </div>
 
-        <Section>
-          <div className='flex flex-1 gap-[20px] mt-[60px]'>
-            <Card
-              number='01.'
-              title='User interviews'
-              description="I interviewed users who used the platform daily. They often couldn't"
-            />
-            <Card
-              number='02.'
-              title='Card-sorting'
-              description='Conducted card-sorting excercises with over 30 users to rethink the product architecture'
-            />
-            <Card
-              number='03.'
-              title='Data analysis'
-              description='Gathered and analyzes data to understand how the users used the platform.'
-            />
-            <Card
-              number='04.'
-              title='User interviews'
-              description='Interviews with main stakeholders to understand business decisions and the vision of the product.'
-            />
-          </div>
-        </Section>
-
-        <div className='mt-[60px] 2xl:max-w-[792px] mx-auto'>
-          <p className='text-medium'>
-            With all that information in mind, the following patterns helped me
-            to drive the next steps.
-          </p>
+        <div className='2xl:max-w-[1168px] mx-auto my-[60px]'>
+          <img
+            src={smartInterviewsUnderstanding}
+            alt='Understanding Smart Interviews'
+          />
         </div>
 
-        <div className='flex 2xl:w-[930px] gap-[20px] mx-auto mt-[60px]'>
-          <StickyNote size='big' color='grey'>
-            Users don’t think in terms of features, they think in terms of
-            actions.
-          </StickyNote>
-          <StickyNote size='big' color='grey'>
-            The existing navigation mirrored internal team structures, not user
-            goals.
-          </StickyNote>
-          <StickyNote size='big' color='grey'>
-            New users in particular struggled to understand what the admin was
-            offering.
-          </StickyNote>
-        </div>
-      </Section>
-
-      <Section fluid className='py-[120px]'>
-        <Section>
-          <h3 className='text-large font-medium mb-[60px]'>Designing</h3>
-        </Section>
-        <Box color='white' className='flex justify-center'>
-          <img src={designingImage3} alt='Designing Tiendanube' />
-        </Box>
-        <Section className='mt-[60px] 2xl:max-w-[792px] mx-auto'>
-          <h4 className='text-medium2 font-medium'>
-            Reframing the structure around user actions:
-          </h4>
-          <p className='text-medium'>
-            The most important design decision in this project wasn’t visual, it
-            was conceptual. Instead of organizing the admin based on internal
-            teams or feature categories, we restructured it around what users
-            actually come to do. We grouped functionality under verbs. This were
-            carefully chosen looking for clear, purposeful actions like:
-          </p>
+        <div className='2xl:max-w-[792px] mx-auto'>
+          <p className='text-medium mt-[20px]'>I learned that:</p>
           <ul className='ml-[40px]'>
             <li className='text-medium'>
-              Administrar (Manage): orders, inventory, clients
+              Recruiters often repeat the same questions and evaluations for
+              every candidate in the same job.
             </li>
             <li className='text-medium'>
-              Potenciar (Grow): integrations, selling channels
+              Notes are subjective, inconsistent, and difficult to search
             </li>
             <li className='text-medium'>
-              Personalizar (Customize): themes, layout
+              Reviewing full interview recordings is time-consuming
             </li>
             <li className='text-medium'>
-              Configurar (Configure): shipping, payments, legal
+              Recruiter have to fill long forms giving feedbacks from the
+              candidates, and they should focus in green and red flags
+            </li>
+            <li className='text-medium'>
+              All this information is used to decide wether to have one or
+              another candidate move to the next step
             </li>
           </ul>
           <p className='text-medium mt-[20px]'>
-            This shift simplified the mental model. It helped users find what
-            they were looking for by matching the main action they wanted to
-            achieve. It also invited users to investigate more features within
-            that section if that was the purposeful action they were interested
-            in.
+            With this in mind, we started exploring:
           </p>
-        </Section>
-
-        <Section fluid className='mt-[60px]'>
-          <img
-            className='w-full'
-            src={designingImage4}
-            alt='Designing Tiendanube 2'
-          />
-        </Section>
+          <p className='text-medium font-medium'>
+            How might we use AI to help recruiters synthesize interviews more
+            efficiently without removing human context or judgment?
+          </p>
+        </div>
       </Section>
 
       <Section fluid className='py-[120px]'>
         <div className='2xl:max-w-[792px] mx-auto'>
-          <h3 className='text-large font-medium mb-[40px]'>Wireframing</h3>
+          <div className='mb-[60px]'>
+            <h3 className='text-large font-medium mb-[12px]'>Designing</h3>
+            <p className='text-medium'>
+              With no existing product to work from, I began by generating
+              design hypothesis we could explore through prototypes and
+              discussion.
+            </p>
+
+            <p className='text-medium'>
+              I started by sketching out a few ideas for how the product could
+              work.
+            </p>
+          </div>
+          <div className='flex flex-wrap 2xl:gap-[60px] my-[60px]'>
+            {designingNotes.map((note, index) => (
+              <StickyNote key={index} size='small' color='purple-light'>
+                {note}
+              </StickyNote>
+            ))}
+          </div>
+
           <p className='text-medium'>
-            Once we had the new architecture, I created prototypes to explore
-            different layout ideas and test the flow. We tested the flows with
-            existing users via InVision and Maze, iterating on the feedback
-            before starting to develop it.
+            These hypotheses shaped the structure of the prototype. I focused on
+            workflows that gave the recruiter control, while letting the system
+            reduce repetitive cognitive load.
           </p>
+
+          <div className='mt-[60px]'>
+            <h4 className='text-medium2 font-medium'>Key design decisions:</h4>
+            {keyDesignDecisions.map((decision, index) => (
+              <div key={index} className='mt-[40px]'>
+                <h5 className='text-medium font-medium'>{decision.title}</h5>
+                <p className='text-medium'>{decision.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        <img
-          src={designingImage5}
-          alt='Wireframing Tiendanube'
-          className='w-[85%] mx-auto'
-        />
       </Section>
 
-      <Section fluid className='py-[120px]'>
-        <div className='2xl:max-w-[792px] mx-auto'>
-          <h3 className='text-large font-medium mb-[40px]'>Implementation</h3>
-          <p className='text-medium'>
-            We decided on an MVP to be able to have the first launch in less
-            than 6 weeks. We focused first on high-impact sections: orders,
-            products, and store configuration.
-          </p>
-          <p className='text-medium mt-[40px]'>
-            The new admin was launched gradually, starting with a small segment
-            of users. The team tracked behavior, collected feedback, and made
-            adjustments before scaling it to everyone. Over time, the full admin
-            migrated to the new structure.
-          </p>
+      <div>
+        <Section className='relative w-full' background='grey'>
+          <div className='absolute z-10 top-0 left-0 2xl:w-[1024px] 2xl:h-[1184px] bg-purple-light'></div>
+          <Section
+            fluid
+            className='pt-[120px] relative z-20'
+            style={{ backgroundColor: 'transparent' }}
+          >
+            <Section
+              className='pb-[96px]'
+              style={{ backgroundColor: 'transparent' }}
+            >
+              <div className='flex justify-start'>
+                <img
+                  src={smartInterviewsImage1}
+                  alt='Smart Interviews 1'
+                  className='max-h-[514px]'
+                />
+              </div>
+              <div className='flex mt-[60px]'>
+                <img
+                  src={smartInterviewsImage3}
+                  alt='Smart Interviews 3'
+                  className='2xl:mt-[204px] max-h-[439px]'
+                />
+                <img
+                  src={smartInterviewsImage2}
+                  alt='Smart Interviews 2'
+                  className='max-h-[351px]'
+                />
+              </div>
+              <div className='flex justify-center mt-[60px]'>
+                <img
+                  src={smartInterviewsImage4}
+                  alt='Smart Interviews 4'
+                  className='max-h-[668px]'
+                />
+              </div>
+            </Section>
+          </Section>
+        </Section>
+
+        <div className='my-[80px]'>
+          <div className='2xl:max-w-[792px] mx-auto'>
+            <Box color='purple-light/10'>
+              <h3 className='text-medium font-medium'>Next steps</h3>
+              <p className='text-medium mt-[20px]'>
+                The next steps will be start thinking about a MVP, testing this
+                concept with real users, understanding where the design works
+                and investigating its feasibility. We’re also exploring how this
+                patterns could influence other parts of the platform. Even
+                though this project has not yet been implemented the act of
+                designing became a way to open conversations, align the team,
+                and test ideas.
+              </p>
+
+              <p className='text-medium mt-[20px]'>
+                I learnt that designing with AI is about shaping expectations,
+                building trust, and helping people stay in control.
+              </p>
+            </Box>
+          </div>
         </div>
-        <div className='flex flex-col gap-[60px]'>
-          <img
-            src={tiendanubeImplementation}
-            alt='Tiendanube implementation'
-            className='mx-auto'
-          />
-          <img
-            src={tiendanubeImplementation2}
-            alt='Tiendanube implementation'
-            className='w-full'
-          />
-          <img
-            src={tiendanubeImplementation3}
-            alt='Tiendanube implementation'
-            className='w-[83%] mx-auto'
-          />
-        </div>
-      </Section>
+      </div>
 
       <Section
         className='text-center text-small my-[20px] font-medium'
@@ -237,4 +258,4 @@ function Fingerprint() {
   )
 }
 
-export default Fingerprint
+export default SmartInterviews
